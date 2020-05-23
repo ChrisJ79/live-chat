136eb8bc63cb6978ac936c0a6e2b378c40edf85f
Chris <csjohnson79@icloud.com>
2020-05-23 14:23:44 -0600

    Created Store component and created reducer to advancethe state of the user, message, and topic

---
+++ src/Store.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+
+export const CTX = React.createContext();
+
+
+/*
+
+msg {
+	from: 'user'
+	msg: 'hi'
+	topic: 'general'
+}
+
+state {
+	topic1: [
+		{msg}, {msg}, {msg}, {new}
+	]
+	topic2: [
+		
+	]
+	
+}
+
+*/
+
+const initState = {
+	general: [
+		{from: 'user', msg: 'hi'},
+		{from: 'user', msg: 'hi'},
+		{from: 'user', msg: 'hi'}
+	],
+	topic2: [
+		{from: 'user', msg: 'hi'},
+		{from: 'user', msg: 'hi'},
+		{from: 'user', msg: 'hi'}
+	]
+}
+
+
+function reducer(state, action) {
+	const {from, msg, topic} = action.payload;
+	switch(action.type) {
+		case 'RECEIVE_MESSAGE':
+			return {
+				...state,
+				[topic]: [
+					...state[topic],
+					{ from, msg } 
+				]
+			}
+		default:
+			return state	
+	}
+
+}
+
+
+export default function Store(props) {
+
+	const reducerHook = React.useReducer(reducer, initState);
+
+	return (
+		<CTX.Provider value={reducerHook}>
+			{props.children}
+		</CTX.Provider>
+	)
+}
